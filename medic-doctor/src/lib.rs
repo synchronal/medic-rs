9ac6822fe35ded101108ab23d37e80ff8ced48fb
Eric Saxby <sax@livinginthepast.org>
2023-04-20 21:00:29 -0700

    Doctor puts check remedies into the clipboard

--- medic-doctor/src/lib.rs
+++ medic-doctor/src/lib.rs
@@ -6,6 +6,9 @@ use medic_lib::config::{Check, Manifest};
 use medic_lib::std_to_string;
 use medic_lib::AppResult;
 
+use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
+
 pub fn run_checks(manifest: Manifest) -> AppResult<()> {
     match manifest.doctor {
         Some(doctor) => {
@@ -30,13 +33,16 @@ fn run_check(check: Check) -> AppResult<()> {
             } else {
                 println!("{}\x1b[31;1mFAILED\x1b[0m", (8u8 as char));
                 eprint!("\x1b[0;31m{}\x1b[0m", std_to_string(result.stderr));
-                // let mut error: String = "Check failed!\r\n".to_owned();
-                // error.push_str("Command:\r\n");
-                // error.push_str(&format!("{command:?}\r\n"));
-                println!(
-                    "\x1b[36mPossible remedy: \x1b[0;33m{}\x1b[0m",
-                    std_to_string(result.stdout)
-                );
+                if result.stdout.is_empty() {
+                    let remedy = std_to_string(result.stdout).trim().to_owned();
+                    print!("\x1b[36mPossible remedy: \x1b[0;33m{remedy}\x1b[0m");
+                    print!("  \x1b[32;1m(it's in the clipboard)\x1b[0m\r\n");
+
+                    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+                    ctx.set_contents(remedy).unwrap();
+                } else {
+                    println!("\x1b[0;33mNo remedy suggested.\x1b[0m");
+                }
                 Err("".into())
             }
         }
