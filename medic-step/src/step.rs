d19761450bd4a7df039f3aa757bd83e94f560c0b
Eric Saxby <sax@livinginthepast.org>
2023-04-21 21:35:34 -0700

    medic test runs steps

--- medic-step/src/step.rs
+++ medic-step/src/step.rs
@@ -1,4 +1,7 @@
 use serde::Deserialize;
+use std::collections::HashMap;
+use std::fmt;
+use std::process::Command;
 
 #[derive(Debug, Deserialize)]
 pub enum Step {
@@ -9,7 +12,82 @@ pub enum Step {
 }
 
 #[derive(Debug, Deserialize)]
-pub struct ShellConfig {}
+pub struct ShellConfig {
+    #[serde(default)]
+    pub allow_failure: bool,
+    pub name: String,
+    pub cmd: String,
+}
 
 #[derive(Debug, Deserialize)]
-pub struct StepConfig {}
+pub struct StepConfig {
+    pub name: Option<String>,
+    pub step: String,
+    pub command: Option<String>,
+    pub args: Option<HashMap<String, String>>,
+}
+
+impl Step {
+    pub fn to_command(self) -> Option<Command> {
+        match self {
+            Step::Shell(config) => {
+                let cmd: Vec<&str> = config.cmd.split(' ').collect();
+                if let Some((first, args)) = cmd.split_first() {
+                    let mut command = Command::new(first);
+                    for arg in args {
+                        command.arg(arg);
+                    }
+                    Some(command)
+                } else {
+                    None
+                }
+            }
+            Step::Step(config) => {
+                let mut check_cmd: String = "medic-step-".to_owned();
+                check_cmd.push_str(&config.step);
+                let mut command = Command::new(check_cmd);
+
+                if let Some(subcmd) = config.command {
+                    command.arg(subcmd);
+                }
+                if let Some(args) = config.args {
+                    for (flag, value) in args {
+                        let mut flag_arg = "--".to_owned();
+                        flag_arg.push_str(&flag);
+                        command.arg(flag_arg).arg(value);
+                    }
+                }
+
+                Some(command)
+            }
+        }
+    }
+}
+
+impl fmt::Display for Step {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Step::Shell(config) => {
+                write!(f, "\x1b[36m{:}", config.name)
+            }
+            Step::Step(config) => {
+                if let Some(name) = &config.name {
+                    write!(f, "\x1b[36m{:}\x1b[0m", name)
+                } else {
+                    write!(f, "\x1b[36m{:}", config.step)?;
+                    if let Some(command) = &config.command {
+                        write!(f, ": \x1b[0;36m{}!", command)?;
+                    }
+                    if let Some(args) = &config.args {
+                        write!(f, " \x1b[0;33m(")?;
+                        for value in args.values() {
+                            write!(f, "{value}")?;
+                        }
+                        write!(f, ")")?;
+                    }
+                    write!(f, "\x1b[0m")
+                }
+            }
+        }
+    }
+}
--- medic-test/src/lib.rs
+++ medic-test/src/lib.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 
 use medic_lib::config::Manifest;
+use medic_lib::std_to_string;
 use medic_lib::AppResult;
 use medic_step::Step;
 
@@ -16,6 +17,33 @@ pub fn run_steps(manifest: Manifest) -> AppResult<()> {
     }
 }
 
-fn run_step(_step: Step) -> AppResult<()> {
-    Ok(())
+fn run_step(step: Step) -> AppResult<()> {
+    print!("\x1b[32m• \x1b[0");
+    print!("{step}  …");
+    if let Some(mut command) = step.to_command() {
+        match command.output() {
+            Ok(result) => {
+                if result.status.success() {
+                    println!("{}\x1b[32;1mOK\x1b[0m", (8u8 as char));
+                    Ok(())
+                } else {
+                    println!("{}\x1b[31;1mFAILED\x1b[0m", (8u8 as char));
+                    eprintln!("\x1b[0;31m== Step output ==\x1b[0m\r\n");
+                    eprint!("{}", std_to_string(result.stderr));
+                    Err("".into())
+                }
+            }
+            Err(err) => {
+                println!("{}\x1b[31;1mFAILED\x1b[0m", (8u8 as char));
+                let mut error: String = "Check failed!\r\n".to_owned();
+                error.push_str("Command:\r\n");
+                error.push_str(&format!("{command:?}\r\n"));
+                error.push_str(&format!("Error:\r\n{err:?}"));
+
+                Err(error.into())
+            }
+        }
+    } else {
+        Err("Failed to parse command".into())
+    }
 }
