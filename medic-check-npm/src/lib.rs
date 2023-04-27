4146b3c44d9a93d19df507c6f2dc9f327c49f71b
Eric Saxby <sax@livinginthepast.org>
2023-04-27 08:53:52 -0700

    Move functionality of medic-check-npm to lib; don't panic when cd dir does not exist

--- medic-check-npm/src/lib.rs
+++ medic-check-npm/src/lib.rs
@@ -1,3 +1,80 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 
 pub mod cli;
+
+use medic_lib::std_to_string;
+use medic_lib::CheckResult::{self, CheckError, CheckOk};
+use std::process::Command;
+
+pub fn npm_exists() -> CheckResult {
+    match Command::new("which").args(["npm"]).output() {
+        Ok(which) => {
+            if which.status.success() {
+                CheckOk
+            } else {
+                let stdout = std_to_string(which.stdout);
+                let stderr = std_to_string(which.stderr);
+                CheckError(
+                    "Unable to find npm.".into(),
+                    Some(stdout),
+                    Some(stderr),
+                    Some("asdf install nodejs".into()),
+                )
+            }
+        }
+        Err(_err) => CheckError(
+            "Unable to search for npm. Is `which` in your PATH?".into(),
+            None,
+            None,
+            None,
+        ),
+    }
+}
+
+pub fn packages_installed(cd: Option<String>, prefix: Option<String>) -> CheckResult {
+    let mut check = Command::new("npm");
+    let mut remedy = Command::new("npm");
+    check.arg("ls").arg("--prefer-offline");
+    remedy.arg("install");
+
+    if let Some(path) = cd {
+        if let Ok(expanded) = std::fs::canonicalize(path) {
+            check.current_dir(&expanded);
+            remedy.current_dir(&expanded);
+        } else {
+            return CheckError(
+                "Given a `cd` param to a directory that does not exist.".into(),
+                None,
+                None,
+                None,
+            );
+        }
+    }
+    if let Some(path) = prefix {
+        check.arg("--prefix").arg(&path);
+        remedy.arg("--prefix").arg(&path);
+    }
+
+    match check.output() {
+        Ok(output) => {
+            let stdout = std_to_string(output.stdout);
+            let stderr = std_to_string(output.stderr);
+            if output.status.success() && !stdout.contains("UNMET DEPENDENCY") {
+                CheckOk
+            } else {
+                CheckError(
+                    "NPM dependencies out of date.".into(),
+                    Some(stdout),
+                    Some(stderr),
+                    Some(format!("({remedy:?})")),
+                )
+            }
+        }
+        Err(_err) => CheckError(
+            "Unable to determine which NPM packages are installed.".into(),
+            None,
+            None,
+            Some("asdf install nodejs".into()),
+        ),
+    }
+}
--- medic-check-npm/src/main.rs
+++ medic-check-npm/src/main.rs
@@ -1,80 +1,14 @@
 use medic_check_npm::cli::{CliArgs, Command};
-use medic_lib::std_to_string;
-use medic_lib::CheckResult::{self, CheckError, CheckOk};
-
-use std::process::Command as Cmd;
+use medic_lib::CheckResult::{self, CheckOk};
 
 fn main() -> CheckResult {
     let cli = CliArgs::new();
 
     match cli.command {
-        Command::Exists => npm_exists()?,
-        Command::PackagesInstalled(args) => packages_installed(args.cd, args.prefix)?,
+        Command::Exists => medic_check_npm::npm_exists()?,
+        Command::PackagesInstalled(args) => {
+            medic_check_npm::packages_installed(args.cd, args.prefix)?
+        }
     }
     CheckOk
-}
-
-fn npm_exists() -> CheckResult {
-    match Cmd::new("which").args(["npm"]).output() {
-        Ok(which) => {
-            if which.status.success() {
-                CheckOk
-            } else {
-                let stdout = std_to_string(which.stdout);
-                let stderr = std_to_string(which.stderr);
-                CheckError(
-                    "Unable to find npm.".into(),
-                    Some(stdout),
-                    Some(stderr),
-                    Some("asdf install nodejs".into()),
-                )
-            }
-        }
-        Err(_err) => CheckError(
-            "Unable to search for npm. Is `which` in your PATH?".into(),
-            None,
-            None,
-            None,
-        ),
-    }
-}
-
-fn packages_installed(cd: Option<String>, prefix: Option<String>) -> CheckResult {
-    let mut check = Cmd::new("npm");
-    let mut remedy = Cmd::new("npm");
-    check.arg("ls").arg("--prefer-offline");
-    remedy.arg("install");
-
-    if let Some(path) = cd {
-        let expanded = std::fs::canonicalize(path).unwrap();
-        check.current_dir(&expanded);
-        remedy.current_dir(&expanded);
-    }
-    if let Some(path) = prefix {
-        check.arg("--prefix").arg(&path);
-        remedy.arg("--prefix").arg(&path);
-    }
-
-    match check.output() {
-        Ok(output) => {
-            let stdout = std_to_string(output.stdout);
-            let stderr = std_to_string(output.stderr);
-            if output.status.success() && !stdout.contains("UNMET DEPENDENCY") {
-                CheckOk
-            } else {
-                CheckError(
-                    "NPM dependencies out of date.".into(),
-                    Some(stdout),
-                    Some(stderr),
-                    Some(format!("({remedy:?})")),
-                )
-            }
-        }
-        Err(_err) => CheckError(
-            "Unable to determine which NPM packages are installed.".into(),
-            None,
-            None,
-            Some("asdf install nodejs".into()),
-        ),
-    }
 }
