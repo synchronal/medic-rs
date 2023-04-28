dc44eabea3e0df1476933b71673bc209e06c7bee
Eric Saxby <sax@livinginthepast.org>
2023-04-28 14:27:38 -0700

    Allow medic-update to be run on its own

---
+++ medic-update/src/cli/app.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+use clap_complete::Shell;
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+#[clap(bin_name = "medic update")]
+pub struct CliArgs {
+    /// Path to a file where medic config can be found
+    #[clap(
+        short,
+        long,
+        env = "MEDIC_CONFIG",
+        default_value = "${PWD}/.medic/config.toml"
+    )]
+    pub config: std::path::PathBuf,
+
+    /// Shell to generate completions for
+    #[clap(long, value_enum, value_parser)]
+    pub completion: Option<Shell>,
+}
