60fb53ac13db8e432f70e29ee1a475861c4e158e
Eric Saxby <sax@livinginthepast.org>
2023-04-28 14:01:20 -0700

    medic-step-git functionality in lib

--- medic-step-git/src/lib.rs
+++ medic-step-git/src/lib.rs
@@ -1,3 +1,28 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 
 pub mod cli;
+
+use medic_lib::std_to_string;
+use medic_lib::StepResult::{self, StepError, StepOk};
+
+use std::process::Command;
+
+pub fn run_git_pull() -> StepResult {
+    match Command::new("git").args(["pull", "--rebase"]).output() {
+        Ok(cmd) => {
+            if cmd.status.success() {
+                StepOk
+            } else {
+                let stdout = std_to_string(cmd.stdout);
+                let stderr = std_to_string(cmd.stderr);
+
+                StepError("Git pull error".into(), Some(stdout), Some(stderr))
+            }
+        }
+        Err(_err) => StepError(
+            "Could not run git pull. Is `git` in PATH?".into(),
+            None,
+            None,
+        ),
+    }
+}
--- medic-step-git/src/main.rs
+++ medic-step-git/src/main.rs
@@ -1,33 +1,9 @@
-use medic_lib::std_to_string;
-use medic_lib::StepResult::{self, StepError, StepOk};
-use medic_step_git::cli::{CliArgs, Command as Cmd};
-
-use std::process::Command;
+use medic_lib::StepResult;
+use medic_step_git::cli::{CliArgs, Command};
 
 fn main() -> StepResult {
     let cli = CliArgs::new();
     match cli.command {
-        Cmd::Pull => run_git_pull()?,
-    }
-    StepOk
-}
-
-fn run_git_pull() -> StepResult {
-    match Command::new("git").args(["pull", "--rebase"]).output() {
-        Ok(cmd) => {
-            if cmd.status.success() {
-                StepOk
-            } else {
-                let stdout = std_to_string(cmd.stdout);
-                let stderr = std_to_string(cmd.stderr);
-
-                StepError("Git pull error".into(), Some(stdout), Some(stderr))
-            }
-        }
-        Err(_err) => StepError(
-            "Could not run git pull. Is `git` in PATH?".into(),
-            None,
-            None,
-        ),
+        Command::Pull => medic_step_git::run_git_pull(),
     }
 }
