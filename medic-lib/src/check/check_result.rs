d809353d00ceeee58e4343f1448963bcc19632cc
Eric Saxby <sax@livinginthepast.org>
2023-05-05 21:54:38 -0700

    CheckResult automatically handles json output format

--- medic-lib/src/check/check_result.rs
+++ medic-lib/src/check/check_result.rs
@@ -1,18 +1,37 @@
+use serde::Serialize;
 use std::io::{self, Write};
 use std::ops::{ControlFlow, FromResidual, Try};
 
-#[derive(Debug, Default, Clone, Eq, PartialEq)]
-pub enum CheckResult {
-    #[default]
-    CheckOk,
-    CheckError(String, Option<String>, Option<String>, Option<String>),
+#[derive(Serialize)]
+struct CheckJson {
+    output: String,
+    error: Option<String>,
+    remedy: Option<String>,
 }
 
-impl std::process::Termination for CheckResult {
-    fn report(self) -> std::process::ExitCode {
+enum CheckResultFormat {
+    Json,
+    Stdio,
+}
+
+impl CheckResultFormat {
+    fn from_env() -> Self {
+        let format = std::env::var("MEDIC_OUTPUT_FORMAT").unwrap_or("json".to_owned());
+        match format.as_str() {
+            "stdio" => Self::Stdio,
+            _ => Self::Json,
+        }
+    }
+
+    fn print(
+        self,
+        msg: String,
+        stdout: Option<String>,
+        stderr: Option<String>,
+        remedy: Option<String>,
+    ) {
         match self {
-            CheckResult::CheckOk => std::process::ExitCode::from(0),
-            CheckResult::CheckError(msg, stdout, stderr, remedy) => {
+            CheckResultFormat::Stdio => {
                 eprintln!("\x1b[31;1mError:\x1b[0m {msg}\r\n");
                 if let Some(stdout) = stdout {
                     if !stdout.is_empty() {
@@ -28,7 +47,47 @@ impl std::process::Termination for CheckResult {
                 if let Some(remedy) = remedy {
                     println!("{remedy}");
                 }
+            }
+            CheckResultFormat::Json => {
+                let mut output = format!("\x1b[31;1mError: \x1b[0m {msg}");
+                let mut error = None;
 
+                if let Some(stdout) = stdout {
+                    if !stdout.is_empty() {
+                        output.push_str(&format!("\r\n\r\n\x1b[31;1mstdout:\x1b[0m\r\n{stdout}"));
+                    }
+                }
+                if let Some(stderr) = stderr {
+                    if !stderr.is_empty() {
+                        error = Some(format!("\x1b[31;1mstdout:\x1b[0m\r\n{stderr}"));
+                    }
+                }
+
+                let json = CheckJson {
+                    output,
+                    error,
+                    remedy,
+                };
+
+                println!("{}", serde_json::to_string(&json).unwrap());
+            }
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, Eq, PartialEq)]
+pub enum CheckResult {
+    #[default]
+    CheckOk,
+    CheckError(String, Option<String>, Option<String>, Option<String>),
+}
+
+impl std::process::Termination for CheckResult {
+    fn report(self) -> std::process::ExitCode {
+        match self {
+            CheckResult::CheckOk => std::process::ExitCode::from(0),
+            CheckResult::CheckError(msg, stdout, stderr, remedy) => {
+                CheckResultFormat::from_env().print(msg, stdout, stderr, remedy);
                 std::process::ExitCode::from(1)
             }
         }
